ada6442348f9f47abe0e3d8e8ea5587e7e04265c
Cameron Smith <smithc11@rpi.edu>
2023-11-21 22:37:50 -0500

    better, but mc is still broken
    the CartesianGridInterpolator was not reproducing the phi field

--- mesh.py
+++ mesh.py
@@ -3,36 +3,21 @@ import numpy as np
 import marching_cubes_2d as mc
 from scipy import ndimage
 import meshio
+import matplotlib.pyplot as plt
+
+from scipy.interpolate import interpn
 
 def mesh_gl(thk, topg, x, y):
-    class CartesianGridInterpolator:
+    class Interp:
         def __init__(self, points, values, method='linear'):
-            self.limits = np.array([[min(x), max(x)] for x in points])
-            self.values = np.asarray(values, dtype=float)
-            self.order = {'linear': 1, 'cubic': 3, 'quintic': 5}[method]
-
-        def __call__(self, x, y):
-            """
-            `xi` here is an array-like (an array or a list) of points.
-
-            Each "point" is an ndim-dimensional array_like, representing
-            the coordinates of a point in ndim-dimensional space.
-            """
-            # transpose the xi array into the ``map_coordinates`` convention
-            # which takes coordinates of a point along columns of a 2D array.
-            xi = np.asarray([[x, y]]).T
-
-            # convert from data coordinates to pixel coordinates
-            ns = self.values.shape
-            coords = [(n - 1) * (val - lo) / (hi - lo)
-                      for val, n, (lo, hi) in zip(xi, ns, self.limits)]
-
-            # interpolate
-            return ndimage.map_coordinates(self.values, coords,
-                                           order=self.order,
-                                           cval=np.nan)[0]  # fill_value
+            self.points = points
+            self.values = values
+            self.method = method
+        def __call__(self,x,y):
+            return interpn(self.points, self.values, (x,y))
 
     print("mesh_gl start\n")
+    assert(thk.shape == (len(y),len(x)))
     tic = time.time()
 
     dx = x[1] - x[0]  # assumed constant and equal in x and y
@@ -46,20 +31,36 @@ def mesh_gl(thk, topg, x, y):
     # corners (minx,miny) and (maxx,miny).
     # In those corners the level set distance will be set to
     # max distance = max(maxx, maxy)
-    (xSize, ySize) = thk.shape
+    (rows, cols) = thk.shape
     max_distance = max(max(x), max(y))
-    phi = np.zeros((xSize, ySize))
+    phi = np.zeros((rows, cols))
     # FIXME this isn't very pythonish
-    for i in range(xSize):
-        for j in range(ySize):
+    for i in range(rows):
+        for j in range(cols):
             if thk[i][j] <= 0:
                 phi[i][j] = max_distance
             else:
                 phi[i][j] = rho_i * thk[i][j] + rho_w * topg[i][j]
-    cgi = CartesianGridInterpolator((x, y), phi, method='linear')
-
+    # plt.imshow(phi, interpolation="nearest", origin="upper")
+    # plt.colorbar()
+    # plt.show()
+    cgi = Interp((x, y), phi.T, method='linear')
+    # The following reproduces the plot of phi above...
+    #  the previous interpolator did not.
+    # interp = np.zeros((rows, cols))
+    # for i in range(rows):
+    #    for j in range(cols):
+    #        d = cgi(x[j], y[i])
+    #        interp[i][j] = d
+    # plt.imshow(interp, interpolation="nearest", origin="upper")
+    # plt.colorbar()
+    # plt.show()
+    mcBegin = time.time()
     edges = mc.marching_cubes_2d(cgi, min(x), max(x), min(y), max(y), dx)
+    mcEnd = time.time()
+    print("marching_cubes_2d done" + str(mcEnd - mcBegin))
 
+    print("len(edges)", len(edges))
     points = []
     for e in edges:
         points.append([e.v1.x, e.v1.y, 0])
